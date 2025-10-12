9639e78e31f83b4395bac5a2c55c62b60ed85c4d
Youssef GestionPro <youssef@gestionpro.com>
2025-10-12 17:39:52 +0100

    fixation build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,11 +5,6 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL
-    }
-  },
   log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error']
 })
 
